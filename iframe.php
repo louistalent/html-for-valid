a8da996cb4f108b9499d4343352f764ca9311203
unknown <louistalent0123@gmail.com>
2022-03-31 09:01:21 +0300

    add files

--- iframe.php
+++ iframe.php
@@ -1,5 +1,12 @@
+
+
+
 <?php
-    $url = "https://app.bogged.finance/bsc/swap?" . $_SERVER['QUERY_STRING'];    
-    $html_select = file_get_contents($url);
-    echo $html_select;
+    $curlSession = curl_init();
+    curl_setopt($curlSession, CURLOPT_URL, 'https://chart.googleapis.com/chart?cht=p3&chs=250x100&chd=t:60,40&chl=Hello|World&chof=json');
+    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+    $jsonData = json_decode(curl_exec($curlSession));
+    curl_close($curlSession);
 ?>
